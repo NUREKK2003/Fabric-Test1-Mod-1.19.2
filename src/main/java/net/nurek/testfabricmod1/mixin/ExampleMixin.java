381d50b36b5a75f157107d569fbaa8d2df95aaad
NUREKK2003 <jakubn20033002@gmail.com>
2022-09-17 22:57:41 +0200

    Initial commit

---
+++ src/main/java/net/nurek/testfabricmod1/mixin/ExampleMixin.java
@@ -0,0 +1,18 @@
+package net.nurek.testfabricmod1.mixin;
+
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.nurek.testfabricmod1.TestMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+
+		TestMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
