a832f428527946e8ec3209f2a8b6efff0532f16a
NUREKK2003 <jakubn20033002@gmail.com>
2022-09-17 22:58:53 +0200

    test commit

--- src/main/java/net/nurek/testfabricmod1/TestMod.java
+++ src/main/java/net/nurek/testfabricmod1/TestMod.java
@@ -15,6 +15,7 @@ public class TestMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		//testowy commit
 
 		LOGGER.info("Hello Fabric world!");
 	}
